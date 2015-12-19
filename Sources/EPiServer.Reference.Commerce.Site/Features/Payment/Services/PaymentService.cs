8abf8b10c77329055c8c969f535ff11d588a4bc6
Jeroen <jeroen.developer@outlook.com>
2015-12-19 20:03:55 +0100

    Cleanup

--- Sources/EPiServer.Reference.Commerce.Site/Features/Payment/Services/PaymentService.cs
+++ Sources/EPiServer.Reference.Commerce.Site/Features/Payment/Services/PaymentService.cs
@@ -11,37 +11,11 @@ using EPiServer.Reference.Commerce.Domain.Contracts.Services;
 namespace EPiServer.Reference.Commerce.Site.Features.Payment.Services
 {
     [ServiceConfiguration(typeof(IPaymentService), Lifecycle = ServiceInstanceScope.Singleton)]
-    public class PaymentService : IPaymentService
+    public class PaymentService : Domain.Services.PaymentService
     {
-        private readonly Func<string, CartHelper> _cartHelper;
-        private readonly LocalizationService _localizationService;
-
         public PaymentService(Func<string, CartHelper> cartHelper, LocalizationService localizationService)
+            : base(cartHelper, localizationService)
         {
-            _cartHelper = cartHelper;
-            _localizationService = localizationService;
-        }
-
-        public void ProcessPayment(IPaymentOption method)
-        {
-            var cart = _cartHelper(Mediachase.Commerce.Orders.Cart.DefaultName).Cart;
-
-            if (!cart.OrderForms.Any())
-            {
-                cart.OrderForms.AddNew();
-            }
-
-            var payment = method.PreProcess(cart.OrderForms[0]);
-
-            if (payment == null)
-            {
-                throw new PreProcessException();
-            }
-
-            cart.OrderForms[0].Payments.Add(payment);
-            cart.AcceptChanges();
-
-            method.PostProcess(cart.OrderForms[0]);
         }
     }
 }
